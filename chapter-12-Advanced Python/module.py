246bf11089e2e7a438b711ded3ec9a25d74a2ad9
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-10 21:27:08 +0530

    practiced some questions

---
+++ chapter-12-Advanced Python/module.py
@@ -0,0 +1,8 @@
+def myfunc():
+    print("hello")
+
+if __name__ == "__main__":
+    # if this code is directly executed by running the file its present in
+    print("we are directly running this code")
+    myfunc()
+    print(__name__)
--- chapter-12-Advanced Python/try-wth-finally.py
+++ chapter-12-Advanced Python/try-wth-finally.py
@@ -1,10 +1,12 @@
 # finally clause ensures execution of a block of code inspective of the exception
+def main():
+    try:
+        a = int(input("enter number: "))
+        print(a)
+    except Exception as e:
+        print(e)
 
-try:
-    a = int(input("enter number: "))
-    print(a)
-except Exception as e:
-    print(e)
+    finally:            # this gets executed regardless of errors
+        print("hi micro")
 
-finally:            # this gets executed regardless of errors
-    print("hi micro")
+main()
