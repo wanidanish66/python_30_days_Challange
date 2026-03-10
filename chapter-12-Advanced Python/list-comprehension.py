dcc7c238081cf577c03dd4101ccbc328c07a12ce
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-10 21:48:44 +0530

    used list comprehension

---
+++ chapter-12-Advanced Python/list-comprehension.py
@@ -0,0 +1,9 @@
+mylist = [1,4,7,2,9]
+
+# squareList = []
+# for i in mylist:
+#     squareList.append(i*i)
+
+#  the same can be done by simply using this 
+squareList = [i * i  for i in mylist]
+print(squareList)
