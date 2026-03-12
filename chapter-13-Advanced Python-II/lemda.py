37e80fabb6fb2917d3dd909736539352c9551702
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-12 23:18:19 +0530

    used lambda function to do simple tasks

---
+++ chapter-13-Advanced Python-II/lemda.py
@@ -0,0 +1,9 @@
+# def square(n):
+    # return n * n           # instead of doing this we can use lemda function like this
+square = lambda x : x * x
+
+print(square(3))     # output 9
+
+sum = lambda a, b, c : a + b + c
+
+print(sum(3,5,6))         # output 14
