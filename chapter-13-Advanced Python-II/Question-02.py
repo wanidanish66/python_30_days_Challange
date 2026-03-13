9fc3955c4546760fbc8f51caa13db42bc76151f5
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-03-13 22:16:31 +0530

    solved question 2 from advanced python II

---
+++ chapter-13-Advanced Python-II/Question-02.py
@@ -0,0 +1,7 @@
+# print student name marks and phoneNo using format function
+
+name = input("enter your name: ")
+marks = int(input("enter your marks: "))
+phoneNo = int(input("enter your phoneNo: "))
+
+print("the name of the student is {} his marks are {} and phone No is {}".format(name,marks,phoneNo))
