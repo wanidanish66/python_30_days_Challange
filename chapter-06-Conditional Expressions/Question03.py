7d7c2db3f89ec8f5f7cfd64c55d42208b3a8511c
wanidanish66 <153353869+wanidanish66@users.noreply.github.com>
2026-04-05 23:25:06 +0530

    changed some solutions

--- chapter-06-Conditional Expressions/Question03.py
+++ chapter-06-Conditional Expressions/Question03.py
@@ -4,9 +4,12 @@ spam = input("enter the comment you want to check:")
 
 if "make a lot of money" in spam :
     print("This is a spam comment.")
+
 elif "buy now" in spam:
         print("This is a spam comment.")
+
 elif "subscribe this" in spam:
         print("This is a spam comment.")
+        
 else:
     print("This is not a spam comment.")
